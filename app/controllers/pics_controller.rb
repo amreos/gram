43b2f1c9e0948b68c3c8f96356b593ff9729ecbf
amre <amr.samier@yahoo.com>
2016-11-23 19:08:04 +0000

    view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,21 @@ def index
 
 end
 
+def new
+
+@pic = Pic.new
+
+
+end
+
+def create
+
+@pic = Pic.new(pic_params)
+
+end
+
+def pic_params
+    params.require(:pic).permit(:description, :title)
+end
+
 end
