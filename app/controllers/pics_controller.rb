d31a92c1f5968eee5c570d6d1c2a78a25ee8a2ea
amre <amr.samier@yahoo.com>
2016-11-29 02:08:36 +0000

    first

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,24 +1,54 @@
 class PicsController < ApplicationController
+    before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
 def index
-
+@pics = Pic.all.order("created_at DESC")
 end
 
+def show
+
+end
 def new
 
-@pic = Pic.new
+@pic = current_user.pics.build
 
 
 end
 
 def create
 
-@pic = Pic.new(pic_params)
+  @pic = current_user.pics.build(pic_params)
+       if @pic.save
+        redirect_to @pic, notice: "Cool you did it!"
+       else
+        render 'new'
+       end
+end
+
+def edit
 
 end
 
+def update
+    if @pic.update(pic_params)
+        redirect_to @pic, notice: "update succesful"
+    else
+        render 'edit'
+    end
+end
+def destroy
+   @pic.destroy
+   redirect_to root_path
+end
+
+
+ private
 def pic_params
     params.require(:pic).permit(:description, :title)
 end
 
+def find_pic
+   @pic = Pic.find(params[:id])
+ 
+end
 end
